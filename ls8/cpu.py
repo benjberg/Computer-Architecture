ba1ebe452c5ef4e1adb0408afa7c6cfbe0b92700
Ben <bbjberg91@gmail.com>
2020-08-18 17:14:41 -0400

    dayone

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,17 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+         # create the ram, registers and program-counter
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
+        self.running = True
+
+    def ram_read(self, address):
+        return self.ram[address]
+
+    def ram_write(self, address, value):
+        self.ram[address] = value
 
     def load(self):
         """Load a program into memory."""
@@ -29,6 +39,8 @@ class CPU:
         for instruction in program:
             self.ram[address] = instruction
             address += 1
+    
+
 
 
     def alu(self, op, reg_a, reg_b):
@@ -62,4 +74,28 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        # set variables
+        LDI = 0b10000010
+        PRN = 0b01000111
+        HLT = 0b00000001
+        
+
+        while self.running:
+            ir = self.ram[self.pc]
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            if ir == HLT:
+                # exit
+                self.running = False
+            elif ir == LDI:
+                # Set the value of a register to an integer.
+                self.reg[operand_a] = [operand_b]
+                self.pc += 3
+            elif ir == PRN:
+                # Print numeric value stored in the given register.
+                print(self.reg[operand_a])
+                self.pc += 2
+            else:
+                print(f'unknown instruction {ir} at address {self.pc}')
+                self.running = False
