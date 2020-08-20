e46b5afc84df8521189456a1299eb12088a13f17
Ben <bbjberg91@gmail.com>
2020-08-20 16:36:13 -0400

    day3

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,7 +1,10 @@
 """CPU functionality."""
 
 import sys
-
+# HLT = 0b00000001
+# LDI = 0b10000010 
+# PRN = 0b01000111
+# MUL = 0b10100010
 class CPU:
     """Main CPU class."""
 
@@ -11,13 +14,25 @@ class CPU:
         self.ram = [0] * 256
         self.reg = [0] * 8
         self.pc = 0
+        #stack pointer
+        self.sp = 256
         self.running = True
+        #initiates branchtable
+        self.branchtable = {
+            0b00000001: self.hlt,
+            0b10000010: self.ldi,
+            0b01000111: self.prn,
+            0b10100010: self.mul,
+            0b01000101: self.push,
+            0b01000110: self.pop
+            }
+        
 
-    def ram_read(self, address):
-        return self.ram[address]
+    def ram_read(self, MAR):
+        return self.ram[MAR]
 
-    def ram_write(self, address, value):
-        self.ram[address] = value
+    def ram_write(self, MAR, MDR):
+        self.ram[MAR] = MDR
 
     def load(self,filename):
         """Load a program into memory."""
@@ -71,8 +86,7 @@ class CPU:
             self.reg[reg_a] -= self.reg[reg_b] 
         elif op == "MUL":
             val = self.reg[reg_a] * self.reg[reg_b]
-            self.reg[reg_a] = val
-            self.pc += 3             
+           
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -96,44 +110,72 @@ class CPU:
 
         print()
 
-    def LDI(self, regval, value):
-        self.reg[regval] = value
+    def ldi(self, op_a, op_b):
+        self.reg[op_a] = op_b
+        return(3, True)
 
-    def PRN(self, regval):
-        print(self.reg[regval])
+    def prn(self, op_a, op_b):
+        print(self.reg[op_a])
+        return (2, True)
 
-    def HLT(self):
-        self.running = False
+    def hlt(self, op_a, op_b):
+        return (0, False)
 
-    def MUL(self,reg_a,reg_b):
-        self.alu("MUL",reg_a,reg_b)
+    def mul(self,op_a,op_b):
+        self.alu("MUL",op_a,op_b)
+        return(3, True)
+    def push(self, op_a, op_b):
+        self.sp -= 1
+        value = self.reg[op_a]
+        self.ram[self.sp] = value
+        return (2, True)
+
+    def pop(self, op_a, op_b):
+        pop_value = self.ram[self.sp]
+        reg_address = op_a
+        self.reg[reg_address] = pop_value
+        self.sp += 1
+        return (2, True)
+
 
     def run(self):
         """Run the CPU."""
-
-        HLT = 1
-        LDI = 0b10000010 
-        PRN = 0b01000111
-        MUL = 0b10100010
-
-
         while self.running:
             ir = self.ram_read(self.pc)
-            op_a, op_b = self.ram[self.pc + 1] , self.ram[self.pc + 2]
+            op_a = self.ram_read(self.pc + 1)
+            op_b = self.ram_read(self.pc + 2)
+            try:
+                #set branch table variable to current ir
+                f = self.branchtable[ir]
+                #break down options to avoid keyerror
+                operation_op = f(op_a, op_b)
+                #run
+                running = operation_op[1]
+                #increment pc
+                self.pc += operation_op[0]
+            except:
+                print(f"Error: Instruction {ir} not found")
+                sys.exit(1)
+        
+
+
+        # while self.running:
+        #     ir = self.ram_read(self.pc)
+        #     op_a, op_b = self.ram[self.pc + 1] , self.ram[self.pc + 2]
             
 
-            if ir == HLT:
-                self.HLT()
-            elif ir == LDI:
-                self.LDI(op_a,op_b)
-                self.pc += 3
-            elif ir == PRN:
-                self.PRN(op_a)
-                self.pc += 2
-            elif ir == MUL:
-                self.MUL(op_a,op_b)
-            else:
-                print('instruction error')
+        #     if ir == HLT:
+        #         self.HLT()
+        #     elif ir == LDI:
+        #         self.LDI(op_a,op_b)
+        #         self.pc += 3
+        #     elif ir == PRN:
+        #         self.PRN(op_a)
+        #         self.pc += 2
+        #     elif ir == MUL:
+        #         self.MUL(op_a,op_b)
+        #     else:
+        #         print('instruction error')
         
         # # set variables
         # LDI = 0b10000010
